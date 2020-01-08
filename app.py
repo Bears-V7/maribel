b84ce73a664c6a17f173c745d48ba5c241f396f0
Bears-V7 <lifeispotato@yonsei.ac.kr>
2020-01-08 10:19:14 +0900

    init

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'hifuclub'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['FLASK_ENV'] = 'production'
+
+db = SQLAlchemy(app)
+
+class Zindak(db.Model):
+    __table_name__ = 'zindak'
+    count = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, count, **kwargs):
+        self.count = count
+
+    def __repr__(self):
+        return str(self.count)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/zindak')
+def zindak():
+    return render_template('zindak.html', zindak=Zindak.query.all()[0])
+
+@app.route('/zindak/inc')
+def inc():
+    Zindak.query.first().count = Zindak.query.first().count + 1
+    db.session.commit()
+    return redirect(url_for('zindak'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
