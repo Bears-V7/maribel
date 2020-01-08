6f9d80986f012bf712689b0cb539d42967a2cfd1
Bears-V7 <lifeispotato@yonsei.ac.kr>
2020-01-08 19:06:55 +0900

    comment post added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -29,7 +29,12 @@ def zindak():
     # return render_template('zindak.html', zindak=Zindak.query.all()[0])
     with open('zindak.txt', 'r') as f:
         s = f.read()
-    return render_template('zindak.html', zindak=s)
+    with open('comments.txt', 'r', encoding='utf-8') as f:
+        lst = list(f.read().split('@CSP@'))
+        for i in range(len(lst)-1):
+            raw = list(lst[i].split('@NSP@'))
+            lst[i] = {'name':raw[0], 'text':raw[1]}
+    return render_template('zindak.html', zindak=s, commlist=lst)
 
 @app.route('/zindak/inc')
 def inc():
@@ -41,6 +46,11 @@ def inc():
         f.write(str(int(s)+1))
     return redirect(url_for('zindak'))
 
+@app.route('/zindak/post', methods=['POST'])
+def post():
+    with open('comments.txt', 'a', encoding='utf-8') as f:
+        f.write(request.form['name']+'@NSP@'+request.form['text']+'@CSP@\n')
+    return redirect(url_for('zindak'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
