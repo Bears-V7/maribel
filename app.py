cacb13d4c4c2a43fa07f5506aac36b7b27d180e8
Bears-V7 <lifeispotato@yonsei.ac.kr>
2020-01-08 20:00:13 +0900

    data dir added

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ class Zindak(db.Model):
     def __repr__(self):
         return str(self.count)
 
+datapath = 'D:/home/site/data'
+#datapath = ''
+#로컬에서 테스트할땐 이거 쓰셈
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -27,9 +31,9 @@ def index():
 @app.route('/zindak')
 def zindak():
     # return render_template('zindak.html', zindak=Zindak.query.all()[0])
-    with open('zindak.txt', 'r') as f:
+    with open(datapath+'zindak.txt', 'r') as f:
         s = f.read()
-    with open('comments.txt', 'r', encoding='utf-8') as f:
+    with open(datapath+'comments.txt', 'r', encoding='utf-8') as f:
         lst = list(f.read().split('@CSP@'))
         for i in range(len(lst)-1):
             raw = list(lst[i].split('@NSP@'))
@@ -40,15 +44,15 @@ def zindak():
 def inc():
     # Zindak.query.first().count = Zindak.query.first().count + 1
     # db.session.commit()
-    with open('zindak.txt', 'r') as f:
+    with open(datapath+'zindak.txt', 'r') as f:
         s = f.read()
-    with open('zindak.txt', 'w') as f:
+    with open(datapath+'zindak.txt', 'w') as f:
         f.write(str(int(s)+1))
     return redirect(url_for('zindak'))
 
 @app.route('/zindak/post', methods=['POST'])
 def post():
-    with open('comments.txt', 'a', encoding='utf-8') as f:
+    with open(datapath+'comments.txt', 'a', encoding='utf-8') as f:
         f.write(request.form['name']+'@NSP@'+request.form['text']+'@CSP@\n')
     return redirect(url_for('zindak'))
 
